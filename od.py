7414876644b89887f035daf21ef20ad72a9bcdcc
albertocastrovill <josealberto.castro@udem.edu>
2024-03-20 22:17:30 -0600

    od.py almost complete

--- od.py
+++ od.py
@@ -48,7 +48,7 @@ def rescale_frame(frame:NDArray, percentage:np.intc=20)->NDArray:
     frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
     return frame
 
-
+"""
 def segment_object(cap:cv2.VideoCapture, args:argparse)->None:
 
     # Main loop
@@ -101,6 +101,58 @@ def segment_object(cap:cv2.VideoCapture, args:argparse)->None:
         if key == ord('q') or key == 27:
             print("Programm finished!")
             break
+            
+"""
+
+
+def segment_object(cap:cv2.VideoCapture, args:argparse)->None:
+    # Main loop
+    while cap.isOpened():
+        # Read current frame
+        ret, frame = cap.read()
+
+        if not ret:
+            print("ERROR! - current frame could not be read")
+            break
+
+        frame = rescale_frame(frame, args.frame_resize_percentage)
+        frame = cv2.medianBlur(frame, 5)
+        frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame_threshold = cv2.inRange(frame_HSV, (66, 0, 33), (180, 255, 255))
+        bitwise_AND = cv2.bitwise_and(frame, frame, mask=frame_threshold)
+
+        cnts, _ = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # Encuentra los contornos en la imagen binaria
+        
+        # Si no hay contornos, continúa con la siguiente iteración del bucle
+        if not cnts:
+            continue
+
+        # Encuentra el contorno con la mayor área
+        largest_cnt = max(cnts, key=cv2.contourArea)
+
+        # Obtiene el rectángulo del contorno más grande y ajusta su tamaño si es necesario
+        x, y, w, h = cv2.boundingRect(largest_cnt)  # x, y son las coordenadas del punto superior izquierdo, w, h son el ancho y la altura
+        
+        # Aquí puedes ajustar el tamaño del rectángulo fijo
+        fixed_width, fixed_height = 30, 30  # Tamaño fijo para el rectángulo
+        x_center, y_center = x + w // 2, y + h // 2
+
+        # Ajusta las coordenadas para centrar el rectángulo fijo sobre el contorno más grande
+        x_fixed = max(0, x_center - fixed_width // 2)
+        y_fixed = max(0, y_center - fixed_height // 2)
+
+        # Dibuja el rectángulo fijo
+        cv2.rectangle(frame, (x_fixed, y_fixed), (x_fixed + fixed_width, y_fixed + fixed_height), (0, 255, 0), 2)
+
+        cv2.imshow(window_params['capture_window_name'], frame)
+        cv2.imshow(window_params['detection_window_name'], bitwise_AND)
+        cv2.imshow('rect', frame_threshold)
+
+        key = cv2.waitKey(5)
+        if key == ord('q') or key == 27:
+            print("Program finished!")
+            break
+
                 
 
 def close_windows(cap:cv2.VideoCapture)->None:
