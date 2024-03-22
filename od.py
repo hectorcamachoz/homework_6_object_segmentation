0dd79c5d43183a7632e5f6000c6d0ced2e02dfef
Hector <92065554+hectorcamachoz@users.noreply.github.com>
2024-03-22 00:01:50 -0600

    5th commit: added an if function

--- od.py
+++ od.py
@@ -51,66 +51,68 @@ def rescale_frame(frame:NDArray, percentage:np.intc=20)->NDArray:
 
 
 def segment_object(cap:cv2.VideoCapture, args:argparse)->None:
-
     # Main loop
+    x_center_old = 0
+    y_center_old = 0
     while cap.isOpened():
-
         # Read current frame
         ret, frame = cap.read()
 
-        # Check if the image was correctly captured
         if not ret:
             print("ERROR! - current frame could not be read")
             break
 
-        # Resize current frame
         frame = rescale_frame(frame, args.frame_resize_percentage)
-        
-        # Apply the median filter
-        frame = cv2.medianBlur(frame,5)
-
-        # Convert the current frame from BGR to HSV
+        frame = cv2.medianBlur(frame, 5)
         frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # Apply a threshold to the HSV image
-        frame_threshold = cv2.inRange(frame_HSV, 
-                                      (66, 0, 33),(180, 255, 255))
-
-        # Filter out the grassy region from current frame, but keep the moving object 
+        frame_threshold = cv2.inRange(frame_HSV, (66, 80, 33), (180, 255, 255))
         bitwise_AND = cv2.bitwise_and(frame, frame, mask=frame_threshold)
 
-       
-        contours, _ = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Encuentra los contornos en la imagen binaria
+
+        cnts, _ = cv2.findContours(frame_threshold, 
+                                   cv2.RETR_TREE, 
+                                   cv2.CHAIN_APPROX_SIMPLE) 
         
-        for cnt in contours:
+        # Si no hay contornos, continúa con la siguiente iteración del bucle
+        if not cnts:
+            continue
 
-            x, y, w, h = cv2.boundingRect(cnt)
-       # print(x,y)
-       # print(x + w, y + h)  
-            area = cv2.contourArea(cnt)
-            print(area)
-            if area > 18 and area < 75 :
-                
-                cv2.rectangle(frame, (x-10, y-10), (x + w + 10, y + h + 10), (0, 255, 0), 2)
+        # Encuentra el contorno con la mayor área
+        largest_cnt = max(cnts, key=cv2.contourArea)
+
+        # Obtiene el rectángulo del contorno más grande y ajusta su tamaño si es necesario
+        x, y, w, h = cv2.boundingRect(largest_cnt)  
+        # x, y son las coordenadas del punto superior izquierdo, w, h son el ancho y la altura
+        
+        # Aquí puedes ajustar el tamaño del rectángulo fijo
+        fixed_width, fixed_height = 30, 30  # Tamaño fijo para el rectángulo
+        x_center, y_center = x + w // 2, y + h // 2
+
+        # Ajusta las coordenadas para centrar el rectángulo fijo sobre el contorno más grande
+        
+
+        
+        # Dibuja el rectángulo fijo
+        if x_center - 10  < x_center_old < x_center + 10 or x_center_old == 0 and y_center - 10  < y_center_old < y_center + 10 or y_center_old == 0 :
+
+            x_fixed = max(0, x_center - fixed_width // 2)
+            y_fixed = max(0, y_center - fixed_height // 2)
+            cv2.rectangle(frame, (x_fixed, y_fixed), 
+                        (x_fixed + fixed_width, y_fixed + fixed_height), 
+                        (0, 255, 0), 2)
+            x_center_old = x_center
+        
             
-
-            
-       # cv2.drawContours(frame_threshold, cnt, -1 , (0, 255, 0), 2)
-
-
-        # Visualise both the input video and the object detection windows
         cv2.imshow(window_params['capture_window_name'], frame)
         cv2.imshow(window_params['detection_window_name'], bitwise_AND)
-        cv2.imshow('rect',frame_threshold)
+        cv2.imshow('rect', frame_threshold)
 
-        
-        
-
-        # The program finishes if the key 'q' is pressed
         key = cv2.waitKey(5)
         if key == ord('q') or key == 27:
-            print("Programm finished!")
+            print("Program finished!")
             break
+
                 
 
 def close_windows(cap:cv2.VideoCapture)->None:
@@ -127,14 +129,11 @@ def run_pipeline(args:argparse)->None:
     # Initialise video capture
     cap = initialise_camera(args)
 
-    # Configure trackbars for the lowest and highest HSV values
-   # configure_trackbars()
-
     # Process video
-    frame, bitwise_AND = segment_object(cap, args)
-
+    segment_object(cap, args)
+    
     # Close all open windows
-    close_windows()
+    close_windows(cap)
 
 
 
@@ -145,4 +144,9 @@ if __name__=='__main__':
 
     # Run pipeline
     run_pipeline(args)
+            
+
+
+
+
 
